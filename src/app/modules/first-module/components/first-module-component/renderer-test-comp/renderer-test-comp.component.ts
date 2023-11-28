8322306d67f487ac0743efa52dd109e703638717
Karthick508 <karthivkn13@gmail.com>
2023-11-28 22:00:29 +0530

    Fork Join Test Added.

--- src/app/modules/first-module/components/first-module-component/renderer-test-comp/renderer-test-comp.component.ts
+++ src/app/modules/first-module/components/first-module-component/renderer-test-comp/renderer-test-comp.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, forkJoin, interval, map, of } from 'rxjs';
 
 @Component({
   selector: 'app-renderer-test-comp',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./renderer-test-comp.component.scss'],
 })
 export class RendererTestCompComponent {
-  ngOnInit() {}
+  $time!: Observable<Date>;
+  $one!: Observable<String>;
+  $two!: Observable<String>;
+  $three!: Observable<String>;
+  $four!: Observable<String>;
+
+  one!: string;
+
+  ngOnInit() {
+    this.$time = interval(1000).pipe(map(() => new Date()));
+
+    //fork join
+    this.$one = of('one');
+    this.$two = of('two');
+    this.$three = of('three');
+    this.$four = of('four');
+    this.forkJoinTest();
+  }
+
+  forkJoinTest() {
+    forkJoin([this.$one, this.$two, this.$three, this.$four]).subscribe({
+      next: (value) => {
+        this.one = value[0].toString();
+      },
+      complete() {
+        console.log('Complete Forkjoin');
+      },
+      error: (error) => {},
+    });
+  }
 }
